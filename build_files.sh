29077ae757181acea185ebdf90ff1abb3b76e1c8
Karim Gad <93051825+KarimGad02@users.noreply.github.com>
2024-07-18 23:06:33 +0300

    Delete build_files.sh

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-# Install Python and pip
-apt-get update && apt-get install -y python3 python3-pip
-
-# Use python3 and pip3 instead of python and pip
-pip3 install -r requirements.txt
-python3 your_script.py
